403ec3df4aa56bf9a1fc79c1b286822ae4527e43
Martin <martin.guerout@gmail.com>
2021-12-15 00:49:14 +0100

    problème au niveau de la variable calendar on a un int au lieu d'un tableau

---
+++ c_libs/cmake-build-release/CMakeFiles/c_libs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_libs.dir/library.c.obj"
+  "libc_libs.dll"
+  "libc_libs.dll.a"
+  "libc_libs.dll.manifest"
+  "libc_libs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_libs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
